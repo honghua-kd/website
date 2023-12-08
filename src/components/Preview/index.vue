6c98d9fec50d9d77ab3f33ffb6ac5b8c4d254d2b
“zhanghelong” <helong.zhang@utflc.com>
2023-12-08 17:30:19 +0800

    preview

--- src/components/Preview/index.vue
+++ src/components/Preview/index.vue
@@ -1,38 +1,67 @@
 <template>
-  <div>
-    <el-dialog
-      :title="dialogTitle"
-      v-model="dialogVisible"
-      :close-on-click-modal="false"
-      :close-on-press-escape="false"
-    >
-      <el-image :src="fileUrl" v-if="showPic" />
-      <pdfViews v-else :url="fileUrl" scale rotate magnifyingGlass />
-    </el-dialog>
-  </div>
+  <el-dialog
+    v-if="isPdf"
+    :title="props.title"
+    v-model="dialogVisible"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+  >
+    <pdf-viewer :url="props.fileUrl" scale rotate magnifyingGlass />
+  </el-dialog>
+  <el-image-viewer
+    v-if="dialogVisible && !isPdf"
+    :urlList="[props.fileUrl]"
+    @close="closeViewer"
+  />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import type { UploadFile } from 'element-plus'
-import pdfViews from '@toystory/potato'
+import { ref, computed, watch } from 'vue'
+import pdfViewer from '@toystory/potato'
 import '@toystory/potato/dist/style.css'
 
-const dialogTitle = ref<string>('文件预览')
-const dialogVisible = ref<boolean>(false)
-const fileUrl = ref<string>('')
-const showPic = ref<boolean>(false)
-
-/** 打开弹窗 */
-const open = (files: UploadFile) => {
-  dialogVisible.value = true
-  fileUrl.value = files.url
-  if (!fileUrl.value.includes('.pdf')) {
-    showPic.value = true
+const props = withDefaults(
+  defineProps<{
+    modelValue: boolean
+    title: string
+    fileUrl: string
+  }>(),
+  {
+    title: '文件预览'
   }
-}
+)
 
-defineExpose({ open })
+// 处理弹窗显示
+const emit = defineEmits(['update:modelValue'])
+
+const dialogVisible = ref<boolean>(props.modelValue)
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    debugger
+    dialogVisible.value = newValue
+  }
+)
+watch(
+  () => dialogVisible.value,
+  (newValue) => {
+    emit('update:modelValue', newValue)
+  }
+)
+
+// 判断是否PDF
+const pdfReg = /^.+(\.pdf)(\?.+)?$/
+const isPdf = computed(() => {
+  if (props.fileUrl) {
+    return pdfReg.test(props.fileUrl)
+  }
+  return false
+})
+
+const closeViewer = () => {
+  dialogVisible.value = false
+}
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/archive/scanAndVerify/UploadForm.vue
+++ src/views/archive/scanAndVerify/UploadForm.vue
@@ -40,6 +40,46 @@
               </div>
               <span class="tip-choose"> 已选择文件：{{ chooseFileNum }} </span>
             </template>
+            <template #file="{ file }">
+              <div>
+                <img
+                  v-if="isPdf(file.url)"
+                  class="el-upload-list__item-thumbnail"
+                  :src="pdfImg"
+                  alt=""
+                />
+                <el-image
+                  v-else
+                  :src="file.url"
+                  :zoom-rate="1.2"
+                  :max-scale="5"
+                  :min-scale="0.2"
+                  :preview-src-list="[file.url]"
+                  fit="cover"
+                />
+                <span class="el-upload-list__item-actions">
+                  <span
+                    class="el-upload-list__item-preview"
+                    @click="handlePictureCardPreview(file)"
+                  >
+                    <el-icon><zoom-in /></el-icon>
+                  </span>
+                  <!-- <span
+                    v-if="!disabled"
+                    class="el-upload-list__item-delete"
+                    @click="handleDownload(file)"
+                  >
+                    <el-icon><Download /></el-icon>
+                  </span> -->
+                  <span
+                    class="el-upload-list__item-delete"
+                    @click="handleRemove"
+                  >
+                    <el-icon><Delete /></el-icon>
+                  </span>
+                </span>
+              </div>
+            </template>
           </el-upload>
         </el-form-item>
       </el-form>
@@ -52,17 +92,17 @@
         </el-button>
       </template>
     </el-dialog>
-    <Preview ref="previewRef" />
+    <Preview v-model="previewVisible" :fileUrl="previewUrl" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, computed } from 'vue'
-import { UploadFilled } from '@element-plus/icons-vue'
+import { UploadFilled, Delete, ZoomIn } from '@element-plus/icons-vue'
 import Preview from '@/components/Preview/index.vue'
-
-import type { UploadFile } from 'element-plus'
-
+import pdfImg from '@/assets/images/pdf.png'
+import { ElMessage, type UploadFile } from 'element-plus'
+// ElImageViewer
 const dialogTitle = ref<string>('上传车辆登记证')
 const dialogVisible = ref<boolean>(false)
 
@@ -72,7 +112,13 @@ const formParams = reactive({
   fileList: [
     {
       name: 'food.jpeg',
+      fileCode: 'ssa',
+      createTime: '111',
       url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+    },
+    {
+      name: 'food.pdf',
+      url: '/static/demo.pdf'
     }
   ]
 })
@@ -95,9 +141,21 @@ defineExpose({ open })
 const checkHandler = () => {}
 
 // 预览
-const previewRef = ref()
+const previewVisible = ref<boolean>(false)
+const previewUrl = ref<string>('')
+const pdfReg = /^.+(\.pdf)(\?.+)?$/
+const isPdf = (url: string) => {
+  return pdfReg.test(url)
+}
+
 const handlePictureCardPreview = (uploadFile: UploadFile) => {
-  previewRef.value.open(uploadFile)
+  if (!uploadFile.url) {
+    ElMessage.error('读取上传文件URL出错')
+    return
+  }
+  debugger
+  previewVisible.value = true
+  previewUrl.value = uploadFile.url
 }
 
 // 删除
