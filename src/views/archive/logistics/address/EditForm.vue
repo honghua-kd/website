4d6a66e4e2cb08e13ad5c614a547f86aa422f0a1
ex-liuziyi <lzy18715232917@163.com>
2023-12-18 15:01:01 +0800

    update

---
+++ src/views/archive/logistics/address/EditForm.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <el-dialog
+      :title="dialogTitle"
+      v-model="dialogVisible"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+    >
+      <el-form ref="formRef" :model="formParamsRequest" label-width="130px">
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item
+              label="联系人名称:"
+              prop="contractName"
+              :rules="[{ required: true }]"
+            >
+              <el-input
+                v-model="basicInfoForm.contractName"
+                clearable
+                placeholder="请输入联系人名称"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="联系人方式:" prop="contractWay">
+              <el-input
+                v-model="basicInfoForm.contractWay"
+                clearable
+                placeholder="请输入联系人方式"
+              />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="邮箱:" prop="email">
+              <el-input
+                v-model="basicInfoForm.email"
+                clearable
+                placeholder="请输入联系人名称"
+              />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="18">
+            <el-form-item label="地址:" prop="address">
+              <el-input
+                v-model="basicInfoForm.address"
+                clearable
+                placeholder="请输入地址"
+              />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <template #footer>
+        <el-button type="primary"> 新 增 </el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          取 消
+        </el-button>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, computed } from 'vue'
+import { UploadFilled, Delete, ZoomIn } from '@element-plus/icons-vue'
+import { ElMessage, ElForm } from 'element-plus'
+import type { UploadFile, UploadRawFile } from 'element-plus'
+
+const dialogTitle = ref<string>('联系人信息')
+const dialogVisible = ref<boolean>(false)
+const basicInfoForm = reactive({})
+/** 打开弹窗 */
+const open = () => {
+  dialogVisible.value = true
+}
+defineExpose({ open })
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/archive/logistics/register/logisticsRoutes.ts
@@ -0,0 +1,193 @@
+export const logisticsRoutes = [
+  {
+    scanDateTime: '2023-11-04 10:30:56',
+    remark: '顺丰速运 已收取快件',
+    zoneCode: '021V',
+    opCode: '54',
+    stayWhyCode: '',
+    cxPackageStatus: 10,
+    cxPackageMessage: '已揽收'
+  },
+  {
+    scanDateTime: '2023-11-04 11:01:03',
+    remark: '快件运输中',
+    zoneCode: '021V',
+    opCode: '501',
+    stayWhyCode: '',
+    cxPackageStatus: 10,
+    cxPackageMessage: '已揽收'
+  },
+  {
+    scanDateTime: '2023-11-04 11:23:38',
+    remark: '快件已发往【上海浦东康桥店】',
+    zoneCode: '021V',
+    opCode: '407',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 13:16:02',
+    remark: '快件在【上海浦东康桥店】完成分拣，准备发往 【上海浦江中转场】',
+    zoneCode: '021V',
+    opCode: '30',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 13:33:39',
+    remark: '快件离开 【上海浦东康桥店】，已在发往  【上海浦江中转场】 的路上',
+    zoneCode: '021V',
+    opCode: '36',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 14:01:49',
+    remark: '快件到达 【上海浦江中转场】',
+    zoneCode: '021WG',
+    opCode: '31',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 15:14:39',
+    remark: '快件在【上海浦江中转场】完成分拣，准备发往 【上海金山中转场】',
+    zoneCode: '021WG',
+    opCode: '30',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 15:18:08',
+    remark: '快件离开 【上海浦江中转场】，已在发往  【上海金山中转场】 的路上',
+    zoneCode: '021WG',
+    opCode: '36',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 20:19:35',
+    remark: '快件到达 【上海金山中转场】',
+    zoneCode: '021WD',
+    opCode: '31',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 20:36:07',
+    remark: '快件在【上海金山中转场】完成分拣，准备发往 【佛山官窑中转场】',
+    zoneCode: '021WD',
+    opCode: '30',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-04 20:43:28',
+    remark: '快件离开 【上海金山中转场】，已在发往  【佛山官窑中转场】 的路上',
+    zoneCode: '021WD',
+    opCode: '36',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-05 17:15:43',
+    remark: '快件到达 【佛山官窑中转场】',
+    zoneCode: '757WA',
+    opCode: '31',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-05 22:31:44',
+    remark: '快件在【佛山官窑中转场】完成分拣，准备发往 【佛山盐步中转场】',
+    zoneCode: '757WA',
+    opCode: '30',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-05 22:34:25',
+    remark: '快件离开 【佛山官窑中转场】，已在发往  【佛山盐步中转场】 的路上',
+    zoneCode: '757WA',
+    opCode: '36',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-06 02:16:24',
+    remark: '快件到达 【佛山盐步中转场】',
+    zoneCode: '757WE',
+    opCode: '31',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-06 04:56:26',
+    remark: '快件在【佛山盐步中转场】完成分拣，准备发往 【佛山南海甘蕉店】',
+    zoneCode: '757WE',
+    opCode: '30',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-06 04:56:50',
+    remark: '快件离开 【佛山盐步中转场】，已在发往  【佛山南海甘蕉店】 的路上',
+    zoneCode: '757WE',
+    opCode: '36',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-06 07:11:23',
+    remark: '快件到达 【佛山南海甘蕉店】',
+    zoneCode: '757AM',
+    opCode: '31',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-06 07:17:00',
+    remark: '我们正在为您的快件分配最合适的快递员，请您稍等。',
+    zoneCode: '757AM',
+    opCode: '44',
+    stayWhyCode: '',
+    cxPackageStatus: 20,
+    cxPackageMessage: '运送中'
+  },
+  {
+    scanDateTime: '2023-11-06 07:27:18',
+    remark:
+      '快件交给陈啟旋，正在派送途中（联系电话：18823151552，顺丰已开启“安全呼叫”保护您的电话隐私,请放心接听！）（主单总件数：1件）',
+    zoneCode: '757AM',
+    opCode: '204',
+    stayWhyCode: '',
+    cxPackageStatus: 30,
+    cxPackageMessage: '派送中'
+  },
+  {
+    scanDateTime: '2023-11-06 07:58:31',
+    remark:
+      '您的快件已签收，如有疑问请电联快递员【陈啟旋，电话：18823151552】，感谢您使用顺丰，期待再次为您服务。（主单总件数：1件）',
+    zoneCode: '757AM',
+    opCode: '80',
+    stayWhyCode: '',
+    cxPackageStatus: 40,
+    cxPackageMessage: '已签收'
+  }
+]
