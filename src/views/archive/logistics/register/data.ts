8862942241d34a6f4f05f37109a4527453a32ed5
ex-liuziyi <lzy18715232917@163.com>
2024-01-02 11:17:45 +0800

    fix: 非空校验

---
+++ src/views/archive/logistics/register/data.ts
@@ -0,0 +1,124 @@
+export default {
+  tableConfig: [
+    {
+      label: '快递状态',
+      prop: 'expressStatus',
+      width: 110,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true,
+      showDisabled: false
+    },
+    {
+      label: '快递单号',
+      prop: 'expressNo',
+      width: 160,
+      valueType: '',
+      align: 'left',
+      showOverflowTooltip: true,
+      show: true,
+      showDisabled: false
+    },
+    {
+      label: '快递公司',
+      prop: 'expressCompany',
+      width: 110,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true,
+      showDisabled: false
+    },
+    {
+      label: '寄送/接收',
+      prop: 'expressType',
+      width: 120,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '寄送日期/接收日期',
+      prop: 'sendTime',
+      width: 150,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '快递主要内容',
+      prop: 'expressContentList',
+      width: 120,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '内容备注',
+      prop: 'expressContentRemark',
+      width: 120,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '寄送人',
+      prop: 'sendUser',
+      width: 110,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '收件人',
+      prop: 'receiveUser',
+      width: 150,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '登记时间',
+      prop: 'createTime',
+      width: 110,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '登记人',
+      prop: 'creator',
+      width: 110,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '更新时间',
+      prop: 'updateTime',
+      width: 170,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    },
+    {
+      label: '更新人',
+      prop: 'updater',
+      width: 170,
+      valueType: '',
+      align: 'center',
+      showOverflowTooltip: true,
+      show: true
+    }
+  ]
+}
