01420da865b276c864020224a48ffbc95e58f36f
ex-cuishengnan <690443794@qq.com>
2024-01-23 13:28:29 +0800

    修改脚本

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -28,7 +28,6 @@ declare module 'vue' {
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
-    ElLink: typeof import('element-plus/es')['ElLink']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElRow: typeof import('element-plus/es')['ElRow']
