a5406f464c85b4ce2cd2cdcd5cb6c9a35a2f78ac
“zhanghelong” <helong.zhang@utflc.com>
2024-01-25 15:42:57 +0800

    feature: 首页恢复

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,18 @@
 <template>
-  <DynamicForm :data="data" v-model="query" />
+  <!-- <DynamicForm :data="data" v-model="query" />
   <SearchBarV2
     :data="searchData"
     v-model="searchQuery"
     :visibleRows="2"
     :colNum="4"
     @search="searchHandler"
-    @reset="resetHandler"
-  />
-  <!-- <div class="home-container">
+  /> -->
+  <div class="home-container">
     <div class="app-welcome">
       <h1>欢迎使用</h1>
       <h1>海通恒信运营中台系统</h1>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <script lang="ts">
@@ -22,33 +21,31 @@ export default {
 }
 </script>
 <script setup lang="ts">
-import { data, searchData } from './data'
-import { reactive } from 'vue'
-const query = reactive({
-  string1: '1',
-  string2: '2',
-  string3: '3',
-  string4: '4',
-  string5: '5',
-  string6: '6',
-  string7: '7'
-})
+// import { data, searchData } from './data'
+// import { reactive } from 'vue'
+// const query = reactive({
+//   string1: '1',
+//   string2: '2',
+//   string3: '3',
+//   string4: '4',
+//   string5: '5',
+//   string6: '6',
+//   string7: '7'
+// })
 
-const searchQuery = reactive({
-  searchString1: [new Date(), new Date()],
-  searchString2: '2',
-  searchString3: '3',
-  searchString4: '4',
-  searchString5: '5',
-  searchString6: '6',
-  searchString7: '7'
-})
+// const searchQuery = reactive({
+//   searchString1: [new Date(), new Date()],
+//   searchString2: '2',
+//   searchString3: '3',
+//   searchString4: '4',
+//   searchString5: '5',
+//   searchString6: '6',
+//   searchString7: '7'
+// })
 
-const searchHandler = () => {
-  console.log('searchQuery', searchQuery)
-}
-
-const resetHandler = () => {}
+// const searchHandler = () => {
+//   console.log('searchQuery', searchQuery)
+// }
 </script>
 
 <style lang="scss" scoped>
